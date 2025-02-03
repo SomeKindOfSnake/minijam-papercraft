dc075eab871a917299164492849e2e1214cfd11b
SomeKindOfSnake <kindofasnake@hotmail.com>
2025-02-02 22:37:03 -0500

    sounds again

--- Scripts/Trappe.gd
+++ Scripts/Trappe.gd
@@ -1,9 +1,12 @@
 class_name Trappe extends Node3D
 
+signal trap_open
+
 var openned = false
 
 func open():
 	if not openned:
+		trap_open.emit()
 		openned = true
 		var tween = create_tween()
 		tween.tween_property(self, "rotation_degrees", Vector3(-110, 0, 0), 1.0).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC)
